b31119221fc659ecb525c79bcc343ad6c7d427ec
Matthew Garrison <matthewgarrison@users.noreply.github.com>
2016-12-02 09:11:04 -0500

    Delete Weapon.py

--- Weapon.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Weapon :
-	Type = Enum("Type", "Melee Ranged")
-
-	def Weapon(self, damageBonus, type) :
-		self.damageBonus = damageBonus
-		if type = Type.Melee : self.type = Type.Melee
-		else : self.type = Type.Ranged
